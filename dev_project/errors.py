6b3892dfc040c84e691f63915c3bfa59997d7507
Selivan <sel.ivan1998@mail.ru>
2020-03-03 22:05:58 +0300

    Errors

---
+++ dev_project/errors.py
@@ -0,0 +1,8 @@
+errors = {'MISSING name': -1,
+          'MISSING surname': -2,
+          'MISSING birth_data': -3,
+          'MISSING position': -4,
+          'WRONG birth_data': -5,
+          'WRONG name LENGTH': -6,
+          'WRONG surname LENGTH': -7,
+          'WRONG position LENGTH': -8}
